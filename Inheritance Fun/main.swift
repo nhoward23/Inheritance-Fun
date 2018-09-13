b5571f6d06eac1fd7a574920dc0b94019fd1c36d
Nicole Howard <nicolehoward@Nicoles-MacBook-Pro.local>
2018-09-13 10:12:23 -0700

    Initial Commit

---
+++ Inheritance Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance Fun
+//
+//  Created by Nicole Howard on 9/13/18.
+//  Copyright © 2018 Nicole Howard. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
